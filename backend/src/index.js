ee020e2da670dc7993cd650b581ed376e70ba2ce
ddv2311 <darshitdvyas@gmail.com>
2025-05-12 12:04:24 +0530

    bakend for messaging logic implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.js";
 
 dotenv.config();
 
@@ -9,7 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}/`);
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { signup,login,logout,updateProfile } from "../controllers/authController.js";
+import { signup,login,logout,updateProfile,checkAuth } from "../controllers/authController.js";
 import { protectRoute } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/signup",signup);
@@ -10,5 +11,8 @@ router.post("/login",login);
 router.post("/logout",logout);
 
 router.put("/update-profile",protectRoute,updateProfile);
+
+router.get("/check",protectRoute,checkAuth);
+
 
 export default router;
